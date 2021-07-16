b13a2f80536c753c2e5090e388362b358000c6f2
satriokusbandrio <79823726+satriokusbandrio@users.noreply.github.com>
2021-07-16 14:04:55 +0700

    change connect

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
-<?php $mysqli = mysqli_connect("http://sql477.main-hosting.eu", "u419649373_forlorn", "5U4QJmoo8l", "u419649373_forlorn"); 
+<?php $mysqli = mysqli_connect("sql477.main-hosting.eu", "u419649373_forlorn", "5U4QJmoo8l", "u419649373_forlorn"); 
 
 ?>
