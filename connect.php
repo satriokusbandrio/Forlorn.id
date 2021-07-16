c3b3e08faaf35f02f8ea6e6541e32d6c664f606f
satriokusbandrio <79823726+satriokusbandrio@users.noreply.github.com>
2021-07-16 14:10:07 +0700

    heroku debug mysqli

--- connect.php
+++ connect.php
@@ -1,3 +1,10 @@
-<?php $mysqli = mysqli_connect("sql477.main-hosting.eu", "u419649373_forlorn", "5U4QJmoo8l", "u419649373_forlorn"); 
+<?php 
+
+$server = "sql477.main-hosting.eu";
+$username = "u419649373_forlorn";
+$password = "5U4QJmoo8l";
+$db = "u419649373_forlorn";
+
+$mysqli = new mysqli($server, $username, $password, $db);
 
 ?>
